06cd1fa0f1eea04ba9a4b14984c1d42f7fcde1e1
MAHAPRABU S <115365595+mahaprabusivasamy@users.noreply.github.com>
2023-09-20 23:31:13 +0530

    Update App.js

--- Frontend/Hello world/App.js
+++ Frontend/Hello world/App.js
@@ -1,1 +1,23 @@
+import React from "react-native"
+import {component} from "react"
+import {View,Text, Stylesheet} from "react-native"
+export default class app extends component {
+render(){
 
+return({
+  <View style={styles.main}>
+  <Text style={styles.text}>Hello world</Text>
+       </View>
+})
+  
+}
+}
+const styles=Stylesheet.create({
+
+main:{
+  flex:1,justifyContent:"center"
+},
+  text:{
+  textAlign:"center"
+  }
+})
